22931a7ed9491197203fca71d239df5160bf2873
nathbiswa <biswanroy29@gmail.com>
2026-03-14 23:38:59 +0600

    first commit

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>Countries Here: </h3>
+        </div>
+    );
+};
+
+export default Countries;
